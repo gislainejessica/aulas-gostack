b98af21c8bf2b919d777580f384226dd30fa3ac4
gislainejessica <gislainehjessica@gmail.com>
2020-10-13 22:23:06 -0300

    chore: config babel

---
+++ intro-react/public/bundle.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var some = function some(a, d) {
+  return a + d;
+};
+
+console.log(some(1, 2));
---
+++ intro-react/src/index.js
@@ -0,0 +1,5 @@
+const some = (a, d) => {
+  return a + d;
+}
+
+console.log(some(1, 2));
