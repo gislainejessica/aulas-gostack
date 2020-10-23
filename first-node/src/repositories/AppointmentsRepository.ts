8e5affe1c545549de7f9cc250dce6585f392ea19
gislainejessica <gislainehjessica@gmail.com>
2020-10-23 17:44:54 -0300

    aula de intro ao node

---
+++ first-node/src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,38 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointments';
+
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
+// Conecção entre Rota(Controller) e BD
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public all(): Appointment[] {
+    return this.appointments;
+  }
+
+  // provider: string, date: Date
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment = this.appointments.find((appointment) =>
+      isEqual(date, appointment.date),
+    );
+    return findAppointment || null;
+  }
+}
+
+export default AppointmentsRepository;
